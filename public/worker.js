6dada4786c264ea45c9bd0e73890e560487e87e1
Matyrobbrt <matyrobbrt@gmail.com>
2023-03-18 21:55:06 +0200

    Update hljs... again

--- public/worker.js
+++ public/worker.js
@@ -1,8 +1,8 @@
-import hljs from './highlight.min'
 onmessage = (event) => {
+    importScripts('./highlight.min.js');
     try {
         const lang = event.data[1];
-        const result = lang == "auto" ? hljs.highlightAuto(event.data[0]) : hljs.highlight(event.data[0], {language: event.data[1]});
+        const result = lang == "auto" ? self.hljs.highlightAuto(event.data[0]) : self.hljs.highlight(event.data[0], {language: event.data[1]});
         postMessage(result.value);
     } catch (er) {
         postMessage(null)

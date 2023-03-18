ba6b0a9913f7dec28da870ff14d23308cbe6167f
Matyrobbrt <65940752+Matyrobbrt@users.noreply.github.com>
2023-03-18 19:23:50 +0000

    Deploying to gh-pages from @ Matyrobbrt/file-preview@4ba8d0c47130ffb9af8ee85cc784606cd23fa26c 🚀

--- src/utils/optional.js
+++
@@ -1,9 +0,0 @@
-export default function optional(value) {
-    return {
-        get: value,
-        isEmpty: value === null,
-        or(supplier) {
-            return this.isEmpty ? optional(supplier()) : this
-        }
-    }
-}

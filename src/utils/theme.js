bfe7bf635c6323fbc14fe44b026b5aa4e102132d
Matyrobbrt <matyrobbrt@gmail.com>
2023-03-19 00:45:07 +0200

    Change default theme

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,6 +1,6 @@
 export const LOCAL_STORAGE_THEME = "theme"
 export const THEMES = ["a11y-dark", "a11y-light", "agate", "an-old-hope", "androidstudio", "arduino-light", "arta", "ascetic", "atom-one-dark-reasonable", "atom-one-dark", "atom-one-light", "base16", "brown-paper", "brown-papersq.png", "codepen-embed", "color-brewer", "dark", "default", "devibeans", "docco", "far", "felipec", "foundation", "github-dark-dimmed", "github-dark", "github", "gml", "googlecode", "gradient-dark", "gradient-light", "grayscale", "hybrid", "idea", "intellij-light", "ir-black", "isbl-editor-dark", "isbl-editor-light", "kimbie-dark", "kimbie-light", "lightfair", "lioshi", "magula", "mono-blue", "monokai-sublime", "monokai", "night-owl", "nnfx-dark", "nnfx-light", "nord", "obsidian", "paraiso-dark", "paraiso-light", "pojoaque", "purebasic", "qtcreator-dark", "qtcreator-light", "rainbow", "routeros", "school-book", "shades-of-purple", "srcery", "stackoverflow-dark", "stackoverflow-light", "sunburst", "tokyo-night-dark", "tokyo-night-light", "tomorrow-night-blue", "tomorrow-night-bright", "vs", "vs2015", "xcode", "xt256"]
-export const DEFAULT_THEME = "a11y-dark"
+export const DEFAULT_THEME = "night-owl"
 
 const ls_selectedTheme = "selectedTheme"
 
